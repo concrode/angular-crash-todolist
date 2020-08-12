55aeda0e99a26d103bd3210e5e068f7476b27f4d
concrode <captainshenhua@gmail.com>
2020-08-12 22:34:18 +1000

    Add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+/**
+ * templateUrl can change to './app.component.mytesting.html'
+ */
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.mytesting.html',
   styleUrls: ['./app.component.css']
 })
+
+/**
+ * title is a property in /app.component.html
+ * name is a property in /app.component.mytesting.html
+ */
 export class AppComponent {
   title = 'angular-crash-todolist';
+  name:string = 'hello world'; // Define name is a string type
+
+  constructor() {
+    // As soon as this component loads, in brower console, it prints out 123
+    console.log(123);
+  }
+
 }
